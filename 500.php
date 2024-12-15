11d6886debf55fd28b0b41c79cd7fdd74b281dfd
wyattwas <wyatt.ruehl@mailbox.org>
2024-12-15 20:45:57 +0100

    adding general error page

--- 500.php
+++ 500.php
@@ -9,7 +9,8 @@
 <body>
 <h1 style="margin-top: 0;">Not Found</h1>
 <p>
-The requested URL was not found on this server.<br>
+The server encountered an internal error and was
+unable to complete your request.<br>
 <br>
 If you think this is a mistake, <a href="https://github.com/wyattwas/personal-website/issues/new?title=Error%3A%20Not%20found">make an issue</a> to report it!<br>
 You can also contact the server admin at <a href="mailto: <?php echo $_SERVER['SERVER_ADMIN']; ?>" ><?php echo $_SERVER['SERVER_ADMIN']; ?></a>
