d4eedc3947ca9c70fadf487540b6f2c4aad8e349
wyattwas <wyatt.ruehl@mailbox.org>
2024-12-15 18:41:08 +0100

    adding address to 404 document

--- 404.php
+++ 404.php
@@ -8,10 +8,13 @@
 </head>
 <body>
 <h1 style="margin-top: 0;">Not Found</h1>
-The site you were trying to reach doesn't seem to exist.<br>
-Please check the URL and try again.
-<hr>
+<p>
+The requested URL was not found on this server.<br>
 If you think this is a mistake, <a href="https://github.com/wyattwas/personal-website/issues/new?title=Error%3A%20Not%20found">make an issue</a> to report it!<br>
 You can also contact the server admin at <a href="mailto: <?php echo $_SERVER['SERVER_ADMIN']; ?>" ><?php echo $_SERVER['SERVER_ADMIN']; ?></a>
+</p>
+<hr>
+<address>Apache/2.4.62 (Debian) Server at www.opencodespace.org Port 443</address>
+<address><i><?php echo $_SERVER['SERVER_SOFTWARE']; . ' Server at ' . $_SERVER['SERVER_ADDRESS'] . ' Port ' . $_SERVER['SERVER_PORT'] ?></i></address>
 </body>
 </html>
