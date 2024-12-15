83140087ef794079bf3f21886c1a5c3ab3c0b403
wyattwas <wyatt.ruehl@mailbox.org>
2024-12-15 18:23:03 +0100

    add error document 404

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>404 - Not Found!</title>
+</head>
+<body>
+<h1>404 - Not Found!</h1>
+The site you were trying to reach doesn't seem to exist.<br>
+Please check the URL and try again.
+<hr>
+If you think this is a mistake, <a href="https://github.com/wyattwas/personal-website/issues/new?title=Error%3A%20Not%20found">make an issue</a> to report it!<br>
+You can also contact the server admin at <a href="mailto: <?php echo $_SERVER['SERVER_ADMIN']; ?>" ><?php echo $_SERVER['SERVER_ADMIN']; ?></a>
+</body>
+</html>
