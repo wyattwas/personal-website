c0aaaf3897074b662b494803d76d699d55daccf4
wyattwas <wyatt.ruehl@mailbox.org>
2024-12-15 21:13:09 +0100

    updating error documents to return index.html

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>404 - Not Found!</title>
+    <title>404 - Not Found</title>
 </head>
 <body>
 <h1 style="margin-top: 0;">Not Found</h1>
@@ -14,6 +14,7 @@ The requested URL was not found on this server.<br>
 If you think this is a mistake, <a href="https://github.com/wyattwas/personal-website/issues/new?title=Error%3A%20Not%20found">make an issue</a> to report it!<br>
 You can also contact the server admin at <a href="mailto: <?php echo $_SERVER['SERVER_ADMIN']; ?>" ><?php echo $_SERVER['SERVER_ADMIN']; ?></a>
 </p>
+<a href="index">Go back home</a>
 <hr>
 <address><i><?php echo $_SERVER['SERVER_SOFTWARE'] . ' Server at ' . $_SERVER['SERVER_NAME'] . ' Port ' . $_SERVER['SERVER_PORT']; ?></i></address>
 </body>
--- 500.php
+++ 500.php
@@ -4,7 +4,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>404 - Not Found!</title>
+    <title>500 - Internal Server Error</title>
 </head>
 <body>
 <h1 style="margin-top: 0;">Not Found</h1>
@@ -15,6 +15,7 @@ unable to complete your request.<br>
 If you think this is a mistake, <a href="https://github.com/wyattwas/personal-website/issues/new?title=Error%3A%20Not%20found">make an issue</a> to report it!<br>
 You can also contact the server admin at <a href="mailto: <?php echo $_SERVER['SERVER_ADMIN']; ?>" ><?php echo $_SERVER['SERVER_ADMIN']; ?></a>
 </p>
+<a href="index">Go back home</a>
 <hr>
 <address><i><?php echo $_SERVER['SERVER_SOFTWARE'] . ' Server at ' . $_SERVER['SERVER_NAME'] . ' Port ' . $_SERVER['SERVER_PORT']; ?></i></address>
 </body>
